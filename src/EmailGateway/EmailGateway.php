b09a2afa7d2f6694e23e49c449a049c35eb40e7a
Behrang N <behrangno@gmail.com>
2019-05-31 15:56:02 +0430

    Add EmailGateway to handle all adapters in one class

--- src/EmailGateway/EmailGateway.php
+++ src/EmailGateway/EmailGateway.php
@@ -2,6 +2,39 @@
 
 namespace EmailGateway;
 
+use EmailGateway\Adapter\AdapterInterface;
+
+/**
+ * Class EmailGateway
+ *
+ * @package EmailGateway
+ */
 class EmailGateway
 {
+	/**
+	 * @var AdapterInterface
+	 */
+	private $adapter;
+
+	/**
+	 * EmailGateway constructor.
+	 *
+	 * @param       $adapter could be like: SendGridAdp::class
+	 * @param array $data email sending data
+	 * @param array $config adapter configuration
+	 */
+	public function __construct($adapter, array $data, array $config)
+	{
+		$this->adapter = new $adapter($data, $config);
+	}
+
+	/**
+	 * Send email by selected adapter
+	 *
+	 * @return mixed|array
+	 */
+	public function send()
+	{
+		return $this->adapter->send();
+	}
 }
