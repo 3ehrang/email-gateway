8d202e8156ba5316511184c50d0d5955f92921a5
Behrang N <behrangno@gmail.com>
2019-05-31 12:21:12 +0430

    Resolve adapter interface namespace

--- src/EmailGateway/Adapter/AdapterInterface.php
+++ src/EmailGateway/Adapter/AdapterInterface.php
@@ -3,13 +3,29 @@
  * Adapter Interface
  */
 
-namespace EGateway\Adapter;
+namespace EmailGateway\Adapter;
 
 /**
  * Each Adapter must implement this interface in order to connect with a email platform.
  */
 interface AdapterInterface
 {
+
+	/**
+	 * AdapterInterface constructor.
+	 *
+	 * @param array $data     array of sending data
+	 *                        [
+	 *						    'subject'       => 'email subject',
+	 *                          'from'          => 'sender@example.com',
+	 *                          'fromName'      => 'Sender Name'
+	 *						    'to'            => 'to-person@example.com',
+	 *						    'toName'        => 'Person Name',
+	 *						    'contentType'   => 'text/html' OR 'text/string',
+	 *   						'content'       => 'email body content'
+	 *					      ]
+	 * @param array $config
+	 */
     public function __construct(array $data, array $config);
 
     public function send();
